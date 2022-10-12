185797b7af514a33d50f39af8596250e1c8c35d4
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-10-12 16:47:58 -0400

    load antibody and color with IMGT ColorScheme

--- plugin/Antibodies.py
+++ plugin/Antibodies.py
@@ -1,38 +1,28 @@
+from turtle import color
 import nanome
-from nanome.api.ui import Menu
-from nanome.util import async_callback, Logs
+from nanome.util import async_callback, Logs, enums
 
-from ABlooper import CDR_Predictor
+from abnumber import Chain as AbChain
+from Bio import SeqIO
 
 
 class Antibodies(nanome.AsyncPluginInstance):
 
-    def start(self):
-        self.menu = Menu()
-        self.menu.title = 'Antibodies'
-        self.menu.width = 1
-        self.menu.height = 1
-
-        msg = 'Hello Nanome!'
-        node = self.menu.root.create_child_node()
-        self.label = node.add_new_label(msg)
-        Logs.message(msg)
-
     @async_callback
     async def on_run(self):
-        # Print the number of complexes in the workspace
-        # to the Menu.
-        # comps = await self.request_complex_list()
-        # msg = f'{len(comps)} Complex(es) in Workspace'
-        # Logs.message(msg)
         input_path = "3chn.pdb"
-        output_path = "ABlooper_model.pdb"
+        self.send_files_to_load([input_path])
+        seq = self.get_sequence_from_pdb(input_path)
+        # abchain = AbChain(seq, scheme='imgt')
+        color_scheme = enums.ColorScheme.IMGT
+        self.apply_color_scheme(color_scheme, enums.ColorSchemeTarget.All, False)
+        Logs.message('Done')
 
-        pred = CDR_Predictor(input_path, model='imgt')  # chains = ("H", "L")
-        pred.write_predictions_in_pdb_format(output_path)
-        # self.label.text_value = msg
-        # self.menu.enabled = True
-        # self.update_menu(self.menu)
+    def get_sequence_from_pdb(self, pdb_filepath):
+        with open(pdb_filepath) as handle:
+            sequence = next(SeqIO.parse(handle, "pdb-atom"))
+        seq = sequence.seq.__str__()
+        return seq
 
 
 def main():
