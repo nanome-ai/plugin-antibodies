889e6fdb1ae698add18768eb9bd066a81692570a
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-10-19 14:25:53 -0400

    refactor to highlight all cdr loops

--- plugin/Antibodies.py
+++ plugin/Antibodies.py
@@ -25,7 +25,7 @@ class Antibodies(nanome.AsyncPluginInstance):
         comp = (await self.request_complexes([shallow_comp.index]))[0]
 
         # Set color scheme to IMGT
-        self.set_plugin_list_button(run_btn, 'Applying Color Scheme...', False)
+        self.set_plugin_list_button(run_btn, 'Coloring...', False)
         self.update_structures_shallow(list(comp.atoms))
         self.apply_color_scheme(
             enums.ColorScheme.IMGT,
@@ -35,48 +35,75 @@ class Antibodies(nanome.AsyncPluginInstance):
         comp.set_all_selected(False)
         
         # Highlight and zoom in on cdr3 loop
-        self.set_plugin_list_button(run_btn, 'Finding CDR3...', False)
+        self.set_plugin_list_button(run_btn, 'Finding loops...', False)
+        cdr1_residues = self.get_cdr1_residues(comp)
+        cdr2_residues = self.get_cdr2_residues(comp)
         cdr3_residues = self.get_cdr3_residues(comp)
-        if cdr3_residues:
+        for i, cdr_residues in enumerate([cdr1_residues, cdr2_residues, cdr3_residues]):
             # Add label to middle residue
-            middle_residue = cdr3_residues[len(cdr3_residues)//2]
+            middle_residue = cdr_residues[len(cdr_residues)//2]
             middle_residue.labeled = True
-            middle_residue.label_text = "CDR3"
-            # Select CDR3 residues 
+            middle_residue.label_text = f"CDR{i + 1}"
+            # Select CDR residues 
             self.set_plugin_list_button(run_btn, 'Zooming...', False)
-            for res in cdr3_residues:
+            for res in cdr_residues:
                 for atom in res.atoms:
                     atom.selected = True
-            await self.update_structures_deep([comp])
-            # Zoom to CDR3 loop
-            await self.zoom_on_structures(cdr3_residues)
-            self.set_plugin_list_button(run_btn, 'Run', True)
+        await self.update_structures_deep([comp])
+        # Zoom to CDR3 loop
+        await self.zoom_on_structures(cdr3_residues)
+        self.set_plugin_list_button(run_btn, 'Run', True)
+
+    def get_cdr1_residues(self, comp):
+        cdr_name = 'cdr1'
+        residues = self._get_cdr_residues(comp, cdr_name)
+        return residues
+
+    def get_cdr2_residues(self, comp):
+        cdr_name = 'cdr2'
+        residues = self._get_cdr_residues(comp, cdr_name)
+        return residues
 
     def get_cdr3_residues(self, comp):
+        cdr_name = 'cdr3'
+        residues = self._get_cdr_residues(comp, cdr_name)
+        return residues
+
+    def _get_cdr_residues(self, comp, cdr: str):
+        """Get nanome residues corresponding to provided cdr name.
+        
+        valid cdr names are 'cdr1', 'cdr2', and 'cdr3'
+        """
+        if cdr not in ['cdr1', 'cdr2', 'cdr3']:
+            raise ValueError(f"Invalid cdr name: {cdr}. Valid choices are 'cdr1', 'cdr2', and 'cdr3'")
         pdb_file = tempfile.NamedTemporaryFile(suffix=".pdb", dir=self.temp_dir.name)
         pdb_file = pdb_file.name
         comp.io.to_pdb(path=pdb_file)
 
         seq_str = self.get_sequence_from_pdb(pdb_file)
         abchain = AbChain(seq_str, scheme='imgt')
-        
+
         protein_letters_3to1 = SeqUtils.IUPACData.protein_letters_3to1
-        cdr3_seq = abchain.cdr3_seq
-        cdr3_residues = []
+
+        seq_attr_name = f'{cdr}_seq'
+        cdr_seq = getattr(abchain, seq_attr_name)
+        cdr_residues = []
         for chain in comp.chains:
             chain_seq = ''.join([protein_letters_3to1[res.name.title()] for res in chain.residues])
-            if cdr3_seq in chain_seq:
+            if cdr_seq in chain_seq:
                 residues = list(chain.residues)
-                # Find the subset of the chain that comprises CDR3 Loop
-                for idx in range(len(residues) - len(cdr3_seq) + 1):
-                    residue_sublist = residues[idx:idx+len(cdr3_seq)]
+                # Find the subset of the chain that comprises CDR Loop
+                for idx in range(len(residues) - len(cdr_seq) + 1):
+                    residue_sublist = residues[idx:idx+len(cdr_seq)]
                     residue_sublist_seq = ''.join([
                         protein_letters_3to1[res.name.title()
                     ] for res in residue_sublist])
-                    if residue_sublist_seq == cdr3_seq:
-                        cdr3_residues = residue_sublist
+                    if residue_sublist_seq == cdr_seq:
+                        cdr_residues = residue_sublist
                         break
-        return cdr3_residues
+        return cdr_residues
+
+        
 
     def get_sequence_from_pdb(self, pdb_filepath):
         with open(pdb_filepath) as handle:
