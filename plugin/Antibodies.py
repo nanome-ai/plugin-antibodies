38ac11a84f2f623d4995c9fab2643849a5c0dbbc
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-10-21 16:55:24 -0400

    refactor with proper coloring

--- plugin/Antibodies.py
+++ plugin/Antibodies.py
@@ -42,13 +42,27 @@ class Antibodies(nanome.AsyncPluginInstance):
             self.set_plugin_list_button(run_btn, 'Run', True)
             return
         comp = (await self.request_complexes([shallow_comp.index]))[0]
-        await self.check_chains(comp)
-
-        # await self.highlight_cdr_loops(comp)
+        await self.highlight_cdr_loops(comp)
         self.set_plugin_list_button(run_btn, 'Run', True)
         Logs.debug("Done")
 
-    async def check_chains(self, comp):
+    def validate_antibody(self, comp):
+        # Make sure at least one chain can be parsed with ABChain
+        for chain in comp.chains:
+            seq_str = self.get_sequence_from_struct(chain)
+            try:
+                abchain = AbChain(seq_str, scheme='imgt')
+                if abchain:
+                    return True
+            except ChainParseError as e:
+                continue
+            return False
+
+    async def highlight_cdr_loops(self, comp):
+        if not self.validate_antibody(comp):
+            self.send_notification(enums.NotificationTypes.error, "Selected complex is not an antibody")
+            return
+
         # Make entire complex Grey.
         Logs.debug("Making Complex Grey.")
         for residue in comp.residues:
@@ -65,93 +79,70 @@ class Antibodies(nanome.AsyncPluginInstance):
             if not seq_str:
                 Logs.warning(f"Unable to sequence chain {chain.name}")
                 continue
-            abchain = AbChain(seq_str, scheme='imgt')
+            try:
+                abchain = AbChain(seq_str, scheme='imgt')
+            except ChainParseError as e:
+                Logs.warning(f"Could not parse Chain {chain.name}")
+                continue
+            
             cdr3_seq = abchain.cdr3_seq
             if not cdr3_seq:
                 Logs.debug(f"No CDR3 in chain {chain.name}")
                 continue
-            cdr3_residues = self.get_cdr3_residues(chain)
-            # cdr3_atoms = itertools.chain(*[res.atoms for res in cdr3_residues])
+
+            try:    
+                cdr1_residues = self.get_cdr1_residues(chain)
+                cdr2_residues = self.get_cdr2_residues(chain)
+                cdr3_residues = self.get_cdr3_residues(chain)
+            except ChainParseError as e:
+                Logs.warning(f"Could find cdr loops for Chain {chain.name}")
+                continue
+            
             chain_type = abchain.chain_type
             if chain_type == 'H':
+                cdr1_color = IMGTCDRColorScheme.HEAVY_CDR1.value
+                cdr2_color = IMGTCDRColorScheme.HEAVY_CDR2.value
                 cdr3_color = IMGTCDRColorScheme.HEAVY_CDR3.value
             else:
+                cdr1_color = IMGTCDRColorScheme.LIGHT_CDR1.value
+                cdr2_color = IMGTCDRColorScheme.LIGHT_CDR2.value
                 cdr3_color = IMGTCDRColorScheme.LIGHT_CDR3.value
             
-            # Add label to middle residue
-            cdr_val = f"CDR{chain_type}3"
-            middle_residue = cdr3_residues[(len(cdr3_residues) // 2)]
-            middle_residue.labeled = True
-            middle_residue.label_text = cdr_val
+            cdr_residue_list = [cdr1_residues, cdr2_residues, cdr3_residues]
+            cdr_colors = [cdr1_color, cdr2_color, cdr3_color]
+            
+            i = 0
+            for res_set, cdr_color in zip(cdr_residue_list, cdr_colors):
+                # Add label to middle residue
+                cdr_val = f"CDR{chain_type}{i + 1}"
+                middle_residue = res_set[(len(res_set) // 2)]
+                middle_residue.labeled = True
+                middle_residue.label_text = cdr_val
+                # Set residue and atom colors
+                for res in res_set:
+                    res.ribbon_color = cdr_color
+                    for atom in res.atoms:
+                        atom.color = cdr_color
+                i += 1
+            # Get residues from list of lists
+            residues = list(itertools.chain.from_iterable(cdr_residue_list))
+            self.update_structures_deep(residues)
+            # Zoom to CDR3 loop
+            await self.zoom_on_structures(residues)
 
-            for res in cdr3_residues:
-                if res.ribboned:
-                    res.ribbon_color = cdr3_color
-
-                for atom in res.atoms:
-                    atom.selected = True
-                    atom.color = cdr3_color
-
-            self.update_structures_deep(cdr3_residues)
-
-    async def highlight_cdr_loops(self, comp):
-        Logs.debug("Finding loops...")
-        run_btn = enums.PluginListButtonType.run
-        self.set_plugin_list_button(run_btn, 'Finding loops...', False)
-        try:
-            cdr1_residues = self.get_cdr1_residues(comp)
-            cdr2_residues = self.get_cdr2_residues(comp)
-            cdr3_residues = self.get_cdr3_residues(comp)
-        except ChainParseError as e:
-            msg = "Please make sure selected structure is an antibody."
-            self.send_notification(enums.NotificationTypes.error, msg)
-            self.set_plugin_list_button(run_btn, 'Run', True)
-            return
-
-        # Set color scheme to IMGT after step above confirms complex is an antibody
-        Logs.debug("Coloring complex")
-        self.set_plugin_list_button(run_btn, 'Coloring...', False)
-        self.update_structures_shallow(list(comp.atoms))
-        self.apply_color_scheme(
-            enums.ColorScheme.IMGT,
-            enums.ColorSchemeTarget.All,
-            False)
-
-        Logs.debug("Formatting...")
-        comp.set_all_selected(False)
-        self.set_plugin_list_button(run_btn, 'Formatting...', False)
-        color_scheme = IMGTColorScheme1
-        for i, cdr_residues in enumerate([cdr1_residues, cdr2_residues, cdr3_residues]):
-            # Add label to middle residue
-            cdr_val = f"CDR{i + 1}"
-            middle_residue = cdr_residues[(len(cdr_residues) // 2)]
-            middle_residue.labeled = True
-            middle_residue.label_text = cdr_val
-            # color CDR residues
-            color_hex = getattr(color_scheme, cdr_val)
-            for res in cdr_residues:
-                for atom in res.atoms:
-                    atom.color = Color(whole_num=color_hex)
-        cdr_residues = cdr1_residues + cdr2_residues + cdr3_residues
-        atom_iterchain = itertools.chain(*[res.atoms for res in cdr_residues])
-        self.update_structures_shallow(list(atom_iterchain))
-
-        # Zoom to CDR3 loop
-        await self.zoom_on_structures(cdr1_residues)
-
-    def get_cdr1_residues(self, comp):
+    def get_cdr1_residues(self, struc):
         cdr_name = 'cdr1'
-        residues = self._get_cdr_residues(comp, cdr_name)
+        residues = self._get_cdr_residues(struc, cdr_name)
         return residues
 
-    def get_cdr2_residues(self, comp):
+    def get_cdr2_residues(self, struc):
         cdr_name = 'cdr2'
-        residues = self._get_cdr_residues(comp, cdr_name)
+        residues = self._get_cdr_residues(struc, cdr_name)
         return residues
 
-    def get_cdr3_residues(self, struct):
+    def get_cdr3_residues(self, struc):
         cdr_name = 'cdr3'
-        residues = self._get_cdr_residues(struct, cdr_name)
+        residues = self._get_cdr_residues(struc, cdr_name)
         return residues
 
     def cdr_in_chain(self, chain_seq, cdr) -> bool:
